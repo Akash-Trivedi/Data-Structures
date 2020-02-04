40dece9af8ae7e8db20196ac556e93e5eaa2fa37
Akash Trivedi <56351314+Akash-Trivedi@users.noreply.github.com>
2020-02-04 08:48:50 +0530

    Update llist1001.cpp

--- linked list/llist1001.cpp
+++ linked list/llist1001.cpp
@@ -1,1 +1,60 @@
-gdfgfdgdf
+#include<iostream>
+using namespace std;
+
+struct node{
+    int data;
+    node *next;
+}*START=NULL, *END=START;
+
+void add(int value){
+    struct node *ptr=new struct node;
+    ptr->data=value;
+    ptr->next=NULL;
+    if(START==NULL){
+        START=ptr;
+        END=ptr;
+    } else{
+        END->next=ptr;
+        END=ptr;
+    }
+    
+}
+
+void display(struct node *head){
+    if(START==NULL){
+        cout<<"List is empty";
+    } else{
+        struct node *ptr= START;
+        while(cout<<ptr->data<<" " && ptr->next!=NULL){
+            ptr=ptr->next;
+        }
+    }
+}
+
+int main(void){
+    int choice;
+    do{
+        cout<<"1. Insert new elemnt"<<"\n";
+        cout<<"2. Display List"<<"\n";
+        cout<<"3. Delete Last Element"<<"\n";
+        cout<<"4. Exit"<<"\n";
+        cout<<"Choose from Menu->";
+        cin>>choice;
+
+        switch (choice){
+            int value;
+        case 1:
+        cout<<"Enter the value->";cin>>value;
+        add(value);
+            break;
+        
+        case 2:
+        display(START);
+            break;
+        
+        default:
+            break;
+        }
+    }while(choice>0&&choice<4);
+    return 0;
+}
